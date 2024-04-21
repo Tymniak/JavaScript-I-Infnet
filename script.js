6135fdec11901579333d1f497e1b002a43ba07be
Tymniako <tiago.tymniak@al.infnet.edu.br>
2024-04-21 11:51:48 -0300

    Primeiro Commit da Aula de JS I

---
+++ script.js
@@ -0,0 +1,15 @@
+//input
+
+anoNascimento = 1994
+
+hoje = new Date()
+
+anoAtual = hoje.getFullYear();
+
+idade = anoAtual - anoNascimento
+
+nome = "Tiago"
+
+mensagem = "Sou " + nome + " e tenho " + idade + " anos"
+
+alert(mensagem)
