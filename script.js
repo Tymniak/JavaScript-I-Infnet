f45e26a0371f418af94d74c0ec156909cc5a0c53
tiago <tiago.tymniak@al.infnet.edu.br>
2024-05-07 14:06:43 -0300

    files added

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 //input
+ const SALARIO_ATE_20 = 1000
+ const SALARIO_ACIMA_20 = 2000
+anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
+nome = prompt("Informe o seu nome")
+salarioBase = parseFloat(prompt("Informe o seu salario base"))
+gratificacao = parseFloat(prompt("Informe sua gratificação"))
+bonus = parseFloat(prompt("Informe seu bônus"))
+desconto = parseFloat(prompt("Informe seu desconto"))
 
-anoNascimento = 1994
+salarioLiquido = 0
+adicional = 0
 
+//processamento
 hoje = new Date()
-
-anoAtual = hoje.getFullYear();
-
+anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
+mensagem = "Sou " + nome + " e tenho " + idade + " anos e ganho R$" + salarioLiquido
 
-nome = "Tiago"
+adicional = 1000
+salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
 
-mensagem = "Sou " + nome + " e tenho " + idade + " anos"
-
+//output
 alert(mensagem)
+
+
+
+
